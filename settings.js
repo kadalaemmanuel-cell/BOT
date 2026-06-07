64a50a4297a736070020f36f72694013059570a7
kadalaemmanuel-cell <kadalaemmanuel@icloud.com>
2026-06-07 19:51:38 +0100

    Personalize Roylon Bot settings

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
+  packname: 'Roylon Bot',
+  author: 'Kdealer',
   botName: "Roylon Bot",
   botOwner: 'Kdealer', // Your name
-  ownerNumber: '2348100996979', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  ownerNumber: '2348100996979', // Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: process.env.GIPHY_API_KEY || '',
   commandMode: "private",
-  maxStoreMessages: 20, 
+  maxStoreMessages: 20,
   storeWriteInterval: 10000,
   description: "Abeg no over use am.",
   version: "3.0.5",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/kadalaemmanuel-cell/BOT/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
